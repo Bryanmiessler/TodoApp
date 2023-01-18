0b147a086da70df6447110c55c4b721e1c9f7de6
BryanMurillo <bryanmiessler@gmail.com>
2023-01-18 18:39:41 -0500

    agregando salida modal click bg y texto hacer click

--- src/components/EmptyTodos.js
+++ src/components/EmptyTodos.js
@@ -1,11 +1,12 @@
 import '../styles/EmptyTodos.css';
 
+
 function EmptyTodos(){
   return (
     <div className="p-container">
       <p className='adelante'>Adelante.</p><br/>
-      
       <p className='crea'>¡Crea tu primer To-do!</p>
+      <p className='crea'>Haz click en el boton de añadir.</p>
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,24 @@
 import { createPortal } from 'react-dom';
 import '../styles/Modal.css';
+import { TodoContext } from '../TodoContext/TodoContext';
+import { Fragment, useContext } from 'react';
 
 function Modal({ children }){
 
+  const { setOpenModal } = useContext(TodoContext)
+
+  const onClickModal = ()=>{
+    setOpenModal(prevState => !prevState);
+  }
   return createPortal(
-    <div className="ModalBackground" >
-      {children}
-    </div>,
+    <Fragment>
+    <main className='container'>
+      <div className='container-children'>
+          {children}
+      </div>
+    </main>
+    <div className='ModalBackground' onClick={onClickModal}/>
+    </Fragment>,
     document.querySelector('#modal')
   );
 
